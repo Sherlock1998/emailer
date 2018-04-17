d261343d5fa78e01c66dd5ce6e48e3fd0878ccde
Sherlock1998 <iuncleblade_yj@hotmail.com>
2018-04-17 12:13:51 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.get("/bye", (req, res) => {
+  res.send("bye");
+});
+
+app.listen(PORT);
