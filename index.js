bb078b1d5854078d1919d8e3a9f3d941ec5c90a6
Sherlock1998 <iuncleblade_yj@hotmail.com>
2018-04-28 17:26:47 +0800

    deploy to front end

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ if (process.env.NODE_ENV === 'production') {
   // if it doesn't recognize the route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve('client', 'buildsssss', 'index.html'));
+    res.sendFile(path.resolve('client/build/index.html'));
   });
 }
 
